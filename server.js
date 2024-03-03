6df9011e041dc25d7f448e6e34d9a886ce09e23e
firas mosbahi <firas.mosbehi@insat.ucar.tn>
2024-03-03 15:14:26 +0100

    middlewares

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const {requestLogger, requestTimeCalculator} = require("./middlewares/request-logger");
+const {errorHandlingMiddleware, CustomHttpError} = require("./middlewares/error-handling middleware");
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(requestLogger , requestTimeCalculator)
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!', status: 'Success' });
+    res.status(200).json({ message: 'Hello World!', status: 'Success' });
 });
+
+app.get('/error' , (req,res , next) => {
+    throw CustomHttpError.InternalServerError()
+})
+
+app.use(errorHandlingMiddleware)
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
